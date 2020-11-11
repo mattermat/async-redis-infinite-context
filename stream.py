4d4f388f944763854c8113ba40fc4c4df8867d7d
Mattia Terenzi <m.terenzi92@gmail.com>
2020-11-12 00:34:35 +0100

    Some other types

--- stream.py
+++ stream.py
@@ -1,27 +1,30 @@
 import aioredis  # type: ignore
+from typing import Dict
+from typing import AsyncGenerator
+import asyncio
 
 
 class Stream:
     def __init__(self, stream_name: str) -> None:
         self.stream_name = str(stream_name)
 
-    async def __aenter__(self):
+    async def __aenter__(self) -> Stream:
         self.r = await aioredis.create_redis(
             'redis://localhost'
         )
         self.running = True
         return self
 
-    async def __aexit__(self, exception_type, exception, traceback):
+    async def __aexit__(self, exception_type: str, exception: str, traceback: str) -> None:
         self.r.close()
 
-    async def __aiter__(self):
+    async def __aiter__(self) -> Stream:
         return self
 
     def stop(self) -> None:
         self.running = False
 
-    async def read(self):
+    async def read(self) -> AsyncGenerator[bytes, bytes]:
         while self.running:
             res = await self.r.xread(
                 [self.stream_name],
